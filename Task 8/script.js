96759a89e9426b3f9dececfdecfd518734969c5b
juliasavicke <juliasavicke@gmail.com>
2022-11-15 11:08:08 +0200

    Task 8: complete

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,24 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+class Calculator {
+  constructor(a, b) {}
+  sum(a, b) {
+    return a + b;
+  }
+  subtraction(a, b) {
+    return a - b;
+  }
+  multiplication(a, b) {
+    return a * b;
+  }
+  division(a, b) {
+    return a / b;
+  }
+}
+
+const calc = new Calculator();
+console.log("calc.sum(12, 8); ===", calc.sum(12, 8));
+console.log("calc.subtraction(12, 8); ===", calc.subtraction(12, 8));
+console.log("calc.multiplication(12, 8); ===", calc.multiplication(12, 8));
+console.log("calc.division(12, 8); ===", calc.division(12, 8));
