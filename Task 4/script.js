3547204ae83a957c42ddb12d8f7cac8d7f92cb2e
juliasavicke <juliasavicke@gmail.com>
2022-11-15 10:24:29 +0200

    Task 4: fetch and render html

--- Task 4/script.js
+++ Task 4/script.js
@@ -8,4 +8,35 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = "cars.json";
+const outputEl = document.getElementById("output");
+
+// fetch data
+function getData(from) {
+  return fetch(from)
+    .then((resp) => resp.json())
+    .catch((err) => console.warn("Error in getData()", err));
+}
+
+// get users from endpoint
+function getCars() {
+  return getData(`${ENDPOINT}`).then((data) => data);
+}
+
+getCars().then((cars) => {
+  cars.forEach((brand) => {
+    console.log("brand ===", brand);
+    const brandCardEl = document.createElement("div");
+    brandCardEl.className = "card";
+    outputEl.append(brandCardEl);
+    const brandNameEl = document.createElement("h2");
+    brandNameEl.textContent = brand.brand;
+    const modelListEl = document.createElement("ul");
+    brandCardEl.append(brandNameEl, modelListEl);
+    brand.models.forEach((model) => {
+      const modelEl = document.createElement("li");
+      modelEl.textContent = model;
+      modelListEl.append(modelEl);
+    });
+  });
+});
